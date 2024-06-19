c56ee72f2b045341ed59261c2a8c498ec9400d21
Артём Поташов <a.potashov@region1.ru>
2024-06-19 20:39:24 +0200

    init commit

---
+++ birthday_bot/db.py
@@ -0,0 +1,20 @@
+import os
+
+import psycopg2 as pg
+
+
+conn = pg.connect(
+    f"dbname={os.getenv('DB_NAME')} "
+    f"user={os.getenv('DB_USER')} "
+    f"password={os.getenv('DB_PASSWORD')} "
+    f"host={os.getenv('DB_HOST')}"
+)
+cur = conn.cursor()
+
+
+async def db_initialize():
+    query = """
+    create table account (id serial primary key, first_name varchar(128), last_name varchar(128), comment text null)
+    """
+    cur.execute(query)
+    conn.commit()
---
+++ birthday_bot/main.py
@@ -0,0 +1,34 @@
+import asyncio
+import os
+
+from aiogram import Bot, Dispatcher
+from aiogram.types import BotCommand
+from handler import router
+from db import db_initialize
+
+
+async def on_startup():
+    await db_initialize()
+
+
+async def set_main_menu(bot: Bot):
+    main_menu_commands = [
+        BotCommand(command='/start', description='Прочитать приглашение'),
+        BotCommand(command='/members', description='Посмотреть список участников'),
+        BotCommand(command='/location', description='Локация проведения'),
+        BotCommand(command='/wishes', description='Пожелания')
+    ]
+    await bot.set_my_commands(main_menu_commands)
+
+
+async def main():
+    api_token = os.getenv('API_TOKEN', '')
+    bot = Bot(token=api_token)
+    dp = Dispatcher()
+    await set_main_menu(bot)
+    dp.include_router(router)
+    await dp.start_polling(bot, on_startup=on_startup)
+
+# Start the bot and send broadcast message
+if __name__ == '__main__':
+    asyncio.run(main())
